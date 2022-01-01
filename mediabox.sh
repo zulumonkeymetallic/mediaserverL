c398d0c2a5699295769200606d0e4589e6d56757
zulumonkeymetallic <94960648+zulumonkeymetallic@users.noreply.github.com>
2022-01-01 17:55:09 +0000

    added mediabox.sh
    file to start the docker instances

---
+++ mediabox.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# test Check that script was run not as root or with sudo
+if [ "$EUID" -eq 0 ]
+  then echo "Please do not run this script as root or using sudo"
+  exit
+fi
+
+
+# See if we need to check GIT for updates
+if [ -e .env ]; then
+    # Check for Updated Docker-Compose
+    printf "Checking for update to Docker-Compose (If needed - You will be prompted for SUDO credentials).\\n\\n"
+    onlinever=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep "tag_name" | cut -d ":" -f2 | sed 's/"//g' | sed 's/,//g' | sed 's/ //g')
+    printf "Current online version is: %s \\n" "$onlinever"
+    localver=$(docker-compose -v | cut -d " " -f4 | sed 's/,//g')
+    printf "Current local version is: %s \\n" "$localver"
+    if [ "$localver" != "$onlinever" ]; then
+        sudo curl -s https://api.github.com/repos/docker/compose/releases/latest | grep "browser_download_url" | grep -i -m1 "$(uname -s)"-"$(uname -m)" | cut -d '"' -f4 | xargs sudo curl -L -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+        printf "\\n\\n"
+    else
+        printf "No Docker-Compose Update needed.\\n\\n"
+    fi
+    # Stash any local changes to the base files
+    git stash > /dev/null 2>&1
+    printf "Updating your local copy of Mediabox.\\n\\n"
+    # Pull the latest files from Git
+    git pull
+    # Check to see if this script "mediabox.sh" was updated and restart it if necessary
+    changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+    check_run() {
+        echo "$changed_files" | grep --quiet "$1" && eval "$2"
+    }
+    # Provide a message once the Git check/update  is complete
+    if [ -z "$changed_files" ]; then
+        printf "Your Mediabox is current - No Update needed.\\n\\n"
+    else
+        printf "Mediabox Files Update complete.\\n\\nThis script will restart if necessary\\n\\n"
+    fi
+    # Rename the .env file so this check fails if mediabox.sh needs to re-launch
+    mv .env 1.env
+    read -r -p "Press any key to continue... " -n1 -s
+    printf "\\n\\n"
+    # Run exec mediabox.sh if mediabox.sh changed
+    check_run mediabox.sh "exec ./mediabox.sh"
+fi
